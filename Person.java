fdac84d476d51c10768c3b845b13e4b3b7030b19
1U23CA030DHARANIKA <139735655+23DHARANIKAS@users.noreply.github.com>
2024-02-05 16:20:05 +0000

    pg 1 com

--- Person.java
+++ Person.java
@@ -1,50 +1,27 @@
-class Product {
-    int productId;String productName;double price;
-    // Define instance variables: productId (int), productName (String), price
-    // (double)
-    static int count;
-    Product(){
-       productName=null;
+public class Person {
+     String firstName;
+     String lastName;
+     int age;
+     Person(){
+        firstName="";
+        lastName="";
+        age=0;
+     } 
+     Person(String fName, String lName, int a) {
+        firstName = fName;
+        lastName = lName;
+        age = a;
     }
-    Product(int productId,String productName,double price){
-        this.productId=productId;
-        this.productName=productName;
-        this.price =price;
-        count++;
+     String FullName() {
+        return firstName + " " + lastName;
     }
-     
-    // Static variable for product count
-    // Constructor to initialize product attributes
-        double discount(double Price,double Discount){
-            return ((100-Discount)/100)*Price;
-        }
-    // Calculate and return the discounted price
-    void displayProductInfo(){
-        System.out.println("Product Id : "+productId);
-        System.out.println("Product Name : "+productName);
-        System.out.println("Product Price : "+price);
-    // Display product information
-    // Static method to retrieve total product count
-}
 
-    }
-    
-public class Practical2 {
     public static void main(String[] args) {
-        Product product1=new Product(101,"Laptop",999.99);
-        Product product2=new Product(202,"Smart Phone",499.95);
-        Product product3=new Product(303,"Tablet",299.50);
-        Product calcDiscount=new Product();       
-         // Create three Product objects with different data
-        product1.displayProductInfo();
-        product2.displayProductInfo();
-        product3.displayProductInfo();
-        // Display product information
-        double DiscountedPrice=calcDiscount.discount(product1.price,10);
-        System.out.println("Discounted Price : "+DiscountedPrice);
-        System.out.println(Product.count);
-        // Calculate and display discounted price
-
-        // Display total product count
+        Person person1 = new Person("John", "Doe", 30);
+        Person person2 = new Person("Alice", "Smith", 25);
+        System.out.println("Person 1: " + person1.FullName() );
+        System.out.println("Person 2: " + person2.FullName());
+        double averageAge = (person1.age + person2.age) / 2.0;
+        System.out.println("Average Age: " + averageAge);
     }
 }
