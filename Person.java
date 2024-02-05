a9ce9d7a63715a3907f0b7c2ff4c7ab416f1ad60
1U23CA030DHARANIKA <139735655+23DHARANIKAS@users.noreply.github.com>
2024-02-05 16:16:10 +0000

    pg 1

--- Person.java
+++ Person.java
@@ -1,17 +1,52 @@
-class Person {
-    // Define instance variables: firstName (String), lastName (String), age (int)
+import java.util.Stack;
 
-    // Default constructor
+class Stack1 {
+    int stck[];
+    int top;
 
-    // Parameterized constructor
+    Stack1(int s) {
+        stck = new int[s];
+        top = -1;
+    }
 
-    // Method to get full name
+    void push(int item) {
+        if (top > stck.length - 1) {
+            System.out.println("Stack is full");
+        } else {
+            stck[++top] = item;
+        }
+    }
 
+    int pop() {
+        if (top < 0) {
+            System.out.println("Stack is empty!");
+            return -1; // Or choose an appropriate return value
+        } else {
+            return stck[top--];
+        }
+    }
+
+    int peek() {
+        if (top < 0) {
+            System.out.println("Stack is empty!");
+            return -1; // Or choose an appropriate return value
+        } else {
+            return stck[top];
+        }
+    }
+}
+
+public class Practical3 {
     public static void main(String[] args) {
-        // Create two Person objects with different data
+        Stack1 s1 = new Stack1(4);
+        for (int i = 1; i <= 4; i++)
+            s1.push(i * 5);
 
-        // Display full names
+        System.out.println("Stack Contents:");
+        for (int i = 0; i < 4; i++)
+            System.out.println(s1.pop());
 
-        // Calculate and display the average age
+        System.out.println("Top element: "+s1.peek());
+        
     }
 }
